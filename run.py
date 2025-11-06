c554b4c8415d96596f9f4237756b762cd456fd13
24c02 <163450896+24c02@users.noreply.github.com>
2025-11-06 13:59:40 +0900

    lol

--- run.py
+++ run.py
@@ -5,11 +5,12 @@ from mapping import MAPPING
 
 buttons = {}
 
-for io, key_event in MAPPING:
-    button = Button(io)
-    button.when_pressed = lambda: uinput.emit(key_event, 1)
-    button.when_released = lambda: uinput.emit(key_event, 0)
-    buttons[io] = button
+with uinput.Device(MAPPING.values()) as device:
+    for io, key_event in MAPPING.items():
+        button = Button(io)
+        button.when_pressed = lambda: uinput.emit(key_event, 1)
+        button.when_released = lambda: uinput.emit(key_event, 0)
+        buttons[io] = button
 
-while True:
-    time.sleep(1)
+    while True:
+        time.sleep(1)
