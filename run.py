e22d053daf10a704b972b4ff72ee65b8aaf4cc7a
24c02 <163450896+24c02@users.noreply.github.com>
2025-11-06 14:00:26 +0900

    LMAO

--- run.py
+++ run.py
@@ -8,8 +8,8 @@ buttons = {}
 with uinput.Device(MAPPING.values()) as device:
     for io, key_event in MAPPING.items():
         button = Button(io)
-        button.when_pressed = lambda: uinput.emit(key_event, 1)
-        button.when_released = lambda: uinput.emit(key_event, 0)
+        button.when_pressed = lambda: device.emit(key_event, 1)
+        button.when_released = lambda: device.emit(key_event, 0)
         buttons[io] = button
 
     while True:
