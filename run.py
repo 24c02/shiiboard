a12eab6996ac24bb381653b64e1ca31f03c687b2
24c02 <163450896+24c02@users.noreply.github.com>
2025-11-06 14:13:58 +0900

    fucking lambda closures

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ buttons = {}
 with uinput.Device(MAPPING.values()) as device:
     for io, key_event in MAPPING.items():
         button = Button(io, bounce_time=0.05)
-        button.when_pressed = lambda: device.emit(key_event, 1)
-        button.when_released = lambda: device.emit(key_event, 0)
+        button.when_pressed = lambda event=key_event: device.emit(event, 1)
+        button.when_released = lambda event=key_event: device.emit(event, 0)
         buttons[io] = button
 
     while True:
